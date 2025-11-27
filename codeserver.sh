556df0f3a660ff6719dafceb2ab0f1891d587c96
intelstriker <intelstriker21@gmail.com>
2025-11-27 16:45:24 +0000

    Update codeserver.sh

--- codeserver.sh
+++ codeserver.sh
@@ -1,6 +1,6 @@
-apt install sudo
-apt install curl
-apt install screen
+apt install sudo -y
+apt install curl -y 
+apt install screen -y
 curl -fsSL https://code-server.dev/install.sh | sh
 sed -i 's/^password:.*/password: IntelstrikerDockervpsbot/' /root/.config/code-server/config.yaml
 screen -dmS codeserver bash -c "code-server"
