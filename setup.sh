a4f4afd5da721205513b2fb781f3969b59cc48eb
intelstriker <intelstriker21@gmail.com>
2025-11-29 09:04:14 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+apt update
 apt install -y sudo neofetch wget
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.49.0/fastfetch-linux-amd64.deb && mv fastfetch-linux-amd64.deb fastfetch.deb && apt install ./fastfetch.deb
 echo "Finshed setup script"
