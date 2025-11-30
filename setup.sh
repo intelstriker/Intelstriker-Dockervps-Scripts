eeec7e1d193ac8df156663950152beb9fb9fb3a9
intelstriker <intelstriker21@gmail.com>
2025-11-30 09:31:40 +0000

    Simplify setup script by removing unused commands
    Removed unnecessary export and installation of packages.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
-export DEBIAN_FRONTEND=noninteractive
-
-apt-get update -qq
-apt-get install -yqq sudo neofetch wget
+apt-get update
+apt-get install wget
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.49.0/fastfetch-linux-amd64.deb && mv fastfetch-linux-amd64.deb fastfetch.deb && apt install ./fastfetch.deb
 echo "Finshed setup script"
