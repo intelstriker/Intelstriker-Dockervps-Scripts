0953bdc36bd9dba5aa0f1fdd637803541eb7b6ad
intelstriker <intelstriker21@gmail.com>
2025-11-30 09:06:41 +0000

    Made it faster

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-apt update -y
-apt install -y sudo neofetch wget
+export DEBIAN_FRONTEND=noninteractive
+
+apt-get update -qq
+apt-get install -yqq sudo neofetch wget
 wget https://github.com/fastfetch-cli/fastfetch/releases/download/2.49.0/fastfetch-linux-amd64.deb && mv fastfetch-linux-amd64.deb fastfetch.deb && apt install ./fastfetch.deb
 echo "Finshed setup script"
